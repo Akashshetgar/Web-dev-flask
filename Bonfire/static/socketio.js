91063686a9cca719b02efc7b7daf955833280043
Akashshetgar <akash.shetgar@somaiya.edu>
2021-11-16 21:43:49 +0530

    Features page:_)

--- Bonfire/static/socketio.js
+++ Bonfire/static/socketio.js
@@ -55,14 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Make 'enter' key submit message
-    let msg = document.getElementById("#user_message");
-    msg.addEventListener("keyup", function(event) {
-        event.preventDefault();
-        if (event.key === 'Enter') {
-            document.getElementById("#send_message").click();
-        }
-    });
+
 
     function leaveRoom(room){
         socket.emit('leave', {'username': username, 'room': room});
